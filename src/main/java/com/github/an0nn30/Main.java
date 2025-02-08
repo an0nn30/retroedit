2b1063b6c65e56a6c50c838816d63b1fc188c7ae
Dustin McKennaWatts <dustin_mckenna@outlook.com>
2025-02-07 21:25:40 -0600

    init flatlaf

--- src/main/java/com/github/an0nn30/Main.java
+++ src/main/java/com/github/an0nn30/Main.java
@@ -9,6 +9,7 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
+        FlatDarkLaf.setup();
         SwingUtilities.invokeLater(() -> {
             EditorFrame frame = new EditorFrame();
 
